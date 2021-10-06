2b2756693cc360c9f0ce112fe75cf84618e935d4
lchandru24 <cl@dovercorp.com>
2021-10-06 11:37:06 +0530

    Add project files.

---
+++ eLaptops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eLaptops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
